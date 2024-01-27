44d5c48b7f235ad5e5c894431214224c359a064c
Brian Kirkpatrick <code@tythos.net>
2024-01-26 20:45:04 -0800

    wrangled some string allocations but I think we're ready for the next one

--- src/main.zig
+++ src/main.zig
@@ -1,33 +1,11 @@
 const std = @import("std");
 const util = @import("./util.zig");
 
-/// copies input to output, replacing each sequence of one or more whitespace
-/// characters with a single space
-fn consolidateWhitespace(allocator: std.mem.Allocator, input: []const u8) ![]u8 {
-    var result: []u8 = undefined;
-    var isFirstWhitespace = true;
-    for (input) |char| {
-        if (char == ' ' or char == '\t' or char == '\n') {
-            if (isFirstWhitespace) {
-                result = try util.concat(allocator, result, " ");
-                isFirstWhitespace = false;
-            }
-        } else {
-            isFirstWhitespace = true;
-            const c = try util.c2s(allocator, char);
-            defer allocator.free(c);
-            result = try util.concat(allocator, result, c);
-        }
-    }
-    return result;
-}
-
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{ .verbose_log = true }){};
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
-    const line: []const u8 = try util.STDIN();
-    const consolidated = try consolidateWhitespace(allocator, line);
-    try util.STDOUT("{s}\n", .{consolidated});
+    const line: []const u8 = try util.STDIN(allocator);
     defer allocator.free(line);
-    defer allocator.free(consolidated);
+    const shorter: []const u8 = try util.consolidateString(allocator, line);
+    try util.STDOUT("consolidated line is:\n{s}\n", .{shorter});
 }
--- src/util.zig
+++ src/util.zig
@@ -13,14 +13,18 @@ pub fn STDOUT(comptime str: []const u8, fmt: anytype) !void {
 /// Defines a useful STIN reader that combines the std.io File, buffered
 /// reader, and a blank-string fallback. The string is allocated on up to 1024
 /// bytes of the stack. Reading may fail.
-pub fn STDIN() ![]u8 {
+pub fn STDIN(allocator: std.mem.Allocator) ![]const u8 {
     const stdin = std.io.getStdIn().reader();
     var buffer = std.io.bufferedReader(stdin);
     std.debug.print("> ", .{});
     var msg_buf: [1024]u8 = undefined;
     const result = try buffer.reader().readUntilDelimiterOrEof(&msg_buf, '\n');
     if (result) |line| {
-        return line;
+        // copy into allocated memory (persist unless error)
+        const strbuf = try allocator.alloc(u8, line.len);
+        errdefer allocator.free(strbuf);
+        @memcpy(strbuf, line);
+        return strbuf;
     } else {
         return "";
     }
@@ -45,4 +49,54 @@ pub fn c2s(allocator: std.mem.Allocator, char: u8) ![]u8 {
     const result = try allocator.alloc(u8, 1);
     result[0] = char;
     return result;
+}
+
+/// Returns the length of a string if consecutive whitespaces are consolidated
+/// into a single space.
+fn getConsolidatedLength(input: []const u8) u32 {
+    var result: u32 = 0;
+    var isFirstWhitespace = true;
+    for (input) |char| {
+        if (char == ' ' or char == '\t' or char == '\n') {
+            if (isFirstWhitespace) {
+                result += 1;
+                isFirstWhitespace = false;
+            }
+        } else {
+            isFirstWhitespace = true;
+            result += 1;
+        }
+    }
+    return result;
+}
+
+pub fn consolidateString(allocator: std.mem.Allocator, line: []const u8) ![]const u8 {
+    // allocate line read from STDIN, compute lengths
+    const before = line.len;
+    const after = getConsolidatedLength(line);
+    try STDOUT("consolidated from {} to {} characters (reduction of {})\n", .{ before, after, before - after });
+
+    // allocate "shorter" string from length
+    var shorter = try allocator.alloc(u8, after);
+    errdefer allocator.free(shorter);
+    var i: u32 = 0;
+    var isFirstWhitespace = true;
+
+    // iterate over characters in original string and copy
+    for (line) |char| {
+        if (char == ' ' or char == '\t' or char == '\n') {
+            // if it's whitespace, we only add and increment if it's the first one in a row
+            if (isFirstWhitespace) {
+                shorter[i] = ' ';
+                isFirstWhitespace = false;
+                i += 1;
+            }
+        } else {
+            // if it's not whitespace, we reset the "first" flag, add, and increment
+            isFirstWhitespace = true;
+            shorter[i] = char;
+            i += 1;
+        }
+    }
+    return shorter;
 }
